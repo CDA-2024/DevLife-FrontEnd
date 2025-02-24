a83113bebda26fa3a8a2dc17138c9a4b8daa5583
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2025-02-24 16:21:49 +0100

    [Features] Connect API to MaterialPage (not working now)

--- src/app/pages/materialPage/hooks/useMaterialApi.ts
+++ src/app/pages/materialPage/hooks/useMaterialApi.ts
@@ -1,1 +1,48 @@
-export const useGetMaterial = () => {};
+import { useGet } from "../../../core/hooks/useApi";
+import { ParamsGet } from "../../../core/schemas/ApiService.interface";
+import { ResponseApi } from "../../../core/schemas/response/ResponseApi.interface";
+import { handleApiError } from "../../../core/utils/error.Utils";
+import { validateResponseData } from "../../../core/utils/validation.Utils";
+import { Material } from "../interfaces/Material.interface";
+import { validateMaterial } from "../services/materialService";
+const resource = "materials";
+
+export const useGetMaterial = (
+  params: ParamsGet = {}
+): ResponseApi<Material> => {
+  const response = useGet<Material>(resource, params);
+
+  const { validData, hasInvalidData } = validateResponseData<Material>(
+    response.data,
+    validateMaterial
+  );
+
+  if (hasInvalidData) {
+    response.error = handleApiError(
+      response.error,
+      new Error("Some material data are invalid.")
+    );
+  }
+
+  return { ...response, error: response.error, data: validData };
+};
+
+export const useGetOneMaterial = (
+  params: ParamsGet = {}
+): ResponseApi<Material> => {
+  const response = useGet<Material>(resource, params);
+
+  const { validData, hasInvalidData } = validateResponseData<Material>(
+    response.data,
+    validateMaterial
+  );
+
+  if (hasInvalidData) {
+    response.error = handleApiError(
+      response.error,
+      new Error("Material data is invalid.")
+    );
+  }
+
+  return { ...response, error: response.error, data: validData };
+};
---
+++ src/app/pages/materialPage/interfaces/Material.interface.ts
@@ -0,0 +1,8 @@
+export interface Material {
+  id: number;
+  idMaterialSkill: number;
+  name: string;
+  type: string;
+  description: string;
+  imageUrl: string;
+}
